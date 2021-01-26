49dc2c58c1ba3e96e75ed76fa19ed134017140ed
BayanaSab <bayana90@yahoo.com>
2021-01-26 15:25:58 -0500

    movie1 updated 2

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -2,6 +2,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World, B21");
         System.out.println("Perfect Future as an SDET");
+        System.out.println("IDK what's going on");
         //watching more movie with hubby
     }
 }
